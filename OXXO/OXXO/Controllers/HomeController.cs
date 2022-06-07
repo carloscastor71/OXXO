05dcc20e62b4bc30bedceea5d595ea55a915c063
carloscastor71 <50450983+carloscastor71@users.noreply.github.com>
2022-06-07 12:04:03 -0500

    Primeras pantallas

---
+++ OXXO/OXXO/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OXXO.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OXXO.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public LoginController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
