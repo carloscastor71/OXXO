f6e7480f3d252c2e29d6339e3b580bef688e74e5
msalas1 <104784984+msalas1@users.noreply.github.com>
2022-06-10 17:34:56 -0500

    Login

--- OXXO/OXXO/Controllers/PerfilController.cs
+++ OXXO/OXXO/Controllers/PerfilController.cs
@@ -59,7 +59,7 @@ namespace OXXO.Controllers
             }
 
             string PuestoUsuario = HttpContext.Session.GetString("IdPerfil");
-            //var result = new PermisoController(Configuration).GetPermisoUsuario("Index","Perfil", PuestoUsuario);
+           // var result = new PermisoController(Configuration).GetPermisoUsuario("Index","Perfil", PuestoUsuario);
             //ViewBag.Crear = result.Crear;
             //ViewBag.Editar = result.Editar;
             
@@ -81,7 +81,7 @@ namespace OXXO.Controllers
                     using (SqlConnection connection = new SqlConnection(dbConn))
                     {
                         connection.Open();
-                        using (SqlCommand command= new SqlCommand("sp_CrearPerfil", connection))
+                        using (SqlCommand command= new SqlCommand("SP_CrearPerfil", connection))
                         {
                             command.CommandType = CommandType.StoredProcedure;
                             command.Parameters.AddWithValue("@Nombre",clsPerfil.Nombre);
@@ -111,6 +111,82 @@ namespace OXXO.Controllers
 
         ///PENDIENTE EDIT
 
-        
+        public IActionResult Editar(int IdPerfil) 
+        {
+            Perfil clsPerfil = new Perfil();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(dbConn))
+                {
+                    String consulta = $"SELECT * FROM Perfil WHERE IdPerfil = '{IdPerfil}'";
+                    SqlCommand command = new SqlCommand(consulta,connection);
+                    connection.Open();
+
+                    using (SqlDataReader dr = command.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            clsPerfil.IdPerfil = Convert.ToString(dr["IdPerfil"]);
+                            clsPerfil.Nombre = Convert.ToString(dr["Nombre"]);
+                            clsPerfil.Descripcion = Convert.ToString(dr["Descripcion"]);
+                            clsPerfil.Activo = Convert.ToBoolean(dr["Activo"]);
+                        }
+                    }
+                    connection.Close();
+            }
+                return View(clsPerfil);
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Alert = CommonServices.ShowAlert(Alerts.Danger, ex.Message);
+                return RedirectToAction(nameof(Index), new { alert = ViewBag.Alert });
+            }
+
+            
+        }
+
+        [HttpPost]
+        [ActionName("Editar")]
+        public IActionResult Editar(Perfil clsPefil) 
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    string currentUser = HttpContext.Session.GetInt32("IdUsuario").ToString();
+                    using (SqlConnection connection = new SqlConnection())
+                    {
+                        connection.Open();
+                        using (SqlCommand command = new SqlCommand("SP_EditarPerfil", connection))
+                        {
+                            command.CommandType = CommandType.StoredProcedure;
+                            command.Parameters.AddWithValue("IdPerfil",clsPefil.IdPerfil);
+                            command.Parameters.AddWithValue("Nombre",clsPefil.Nombre);
+                            command.Parameters.AddWithValue("Descripcion",clsPefil.Descripcion);
+                            command.Parameters.AddWithValue("Activo", Convert.ToInt32(clsPefil.Activo));
+                            command.Parameters.AddWithValue("IdUsuarioFUM", currentUser);
+                            command.ExecuteNonQuery();
+                            connection.Close();
+                        }
+                        ViewBag.Alert = CommonServices.ShowAlert(Alerts.Success, "Registro actualizado con éxito.");
+                        return RedirectToAction(nameof(Index), new { alert = ViewBag.Alert});
+                    }
+
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Alert = CommonServices.ShowAlert(Alerts.Danger, ex.Message);
+                return RedirectToAction(nameof(Index), new { alert = ViewBag.Alert });
+            }
+        }
+
+
     }
 }
