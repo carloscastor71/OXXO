41fc756dc09b2f261480aa934ff2cc99fbaf2573
carloscastor71 <50450983+carloscastor71@users.noreply.github.com>
2022-06-14 17:38:27 -0500

    Cambios en el login, Sweet alerts y alta de comercion

--- OXXO/OXXO/Controllers/AltaController.cs
+++ OXXO/OXXO/Controllers/AltaController.cs
@@ -29,80 +29,75 @@ namespace OXXO.Controllers
 
         private readonly ILogger<HomeController> _logger;
 
-        //public ComercioController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-        public IActionResult Index()
+        public IActionResult Index(string? alert)
         {
+            ViewBag.Alert = alert;
             ListadoBancos();
-            //ListadoEstatus();
             ListadoGiroComercial();
             return View();
         }
 
-
-        //public IActionResult Create()
-        //{
-        //    ListadoBancos();
-        //    //ListadoEstatus();
-        //    ListadoGiroComercial();
-        //    return View();
-        //}
-
         [HttpPost]
-        public IActionResult Index( int IdEmisor,string Rfc, string NombreCompleto, string Telefono, string Correo, string Direccion, string CuentaDeposito, int IdBanco , string RazonSocial, string NombreComercial, int IdGiroComercio, string Portal, int Persona, int Usuario_FAl, int Usuario_FUM, int IdTipoDeposito)
+        public IActionResult Index(string Rfc, string NombreCompleto, string Telefono, string Correo, string Direccion, string CuentaDeposito, int IdBanco, string RazonSocial, string NombreComercial, int IdGiroComercio, string Portal, int Persona, int Usuario_FAl, int Usuario_FUM, int IdTipoDeposito)
         {
-            string IdPerfil = HttpContext.Session.GetString("IdPerfil");
-            try
+            int IdPerfil = Int32.Parse(HttpContext.Session.GetString("IdPerfil"));
+            if (String.IsNullOrEmpty(Rfc)|| String.IsNullOrEmpty(Telefono) || String.IsNullOrEmpty(Correo) || String.IsNullOrEmpty(Direccion) || String.IsNullOrEmpty(CuentaDeposito) || String.IsNullOrEmpty(IdBanco.ToString()) || String.IsNullOrEmpty(IdTipoDeposito.ToString()))
             {
-               
-                    string connectionString = Configuration["ConnectionStrings:ConexionString"];
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        connection.Open();
-                        Guid RFC = Guid.NewGuid();
-                        string prOutput = "";
-                        using (SqlCommand command = new SqlCommand("SP_ComercioAlta", connection))
-                        {
-                            command.CommandType = CommandType.StoredProcedure;
-                            command.Parameters.AddWithValue("@IdEmisor", IdEmisor);
-                            command.Parameters.AddWithValue("@RFC", Rfc);
-                            command.Parameters.AddWithValue("@NombreCompleto", NombreCompleto);
-                            command.Parameters.AddWithValue("@Telefono", Telefono);
-                            command.Parameters.AddWithValue("@Correo", Correo);
-                            command.Parameters.AddWithValue("@Direccion", Direccion);
-                            command.Parameters.AddWithValue("@CuentaDeposito", CuentaDeposito);
-                            command.Parameters.AddWithValue("@IdBanco", IdBanco);
-                            command.Parameters.AddWithValue("@RazonSocial", RazonSocial);
-                            command.Parameters.AddWithValue("@NombreComercial", NombreComercial);
-                            command.Parameters.AddWithValue("@IdGiroComercio", IdGiroComercio);
-                            command.Parameters.AddWithValue("@Portal", Portal);
-                            command.Parameters.AddWithValue("@Persona",Persona);
-                            command.Parameters.AddWithValue("@Usuario_FAl", IdPerfil);
-                            command.Parameters.AddWithValue("@Usuario_FUM", IdPerfil);
-                            command.Parameters.AddWithValue("@IdTipoDeposito", IdTipoDeposito);
-                            command.ExecuteNonQuery();
-                            connection.Close();
-                            ViewBag.Alert = CommonServices.ShowAlert(Alerts.Success, "El comercio se ha dado de alta correctamente.");
-                            return RedirectToAction(nameof(Index), new { alert = ViewBag.Alert });
-                        }
-                    }
-           
-            }
-            catch (Exception ex)
-            {
-                ViewBag.Alert = CommonServices.ShowAlert(Alerts.Danger, ex.Message);
+                ViewBag.Alert = CommonServices.ShowAlert(Alerts.Warning, "Tienes campos sin llenar.");
                 return RedirectToAction(nameof(Index), new { alert = ViewBag.Alert });
             }
-           
+            else
+            {
+                try
+                {
+                    string connectionString = Configuration["ConnectionStrings:ConexionString"];
+                    using SqlConnection connection = new SqlConnection(connectionString);
+
+                    connection.Open();
+                    Guid RFC = Guid.NewGuid();
+
+                    using SqlCommand command = new SqlCommand("SP_ComercioAlta", connection);
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@RFC", Rfc);
+                    command.Parameters.AddWithValue("@NombreCompleto", NombreCompleto);
+                    command.Parameters.AddWithValue("@Telefono", Telefono);
+                    command.Parameters.AddWithValue("@Correo", Correo);
+                    command.Parameters.AddWithValue("@Direccion", Direccion);
+                    command.Parameters.AddWithValue("@CuentaDeposito", CuentaDeposito);
+                    command.Parameters.AddWithValue("@IdBanco", IdBanco);
+                    command.Parameters.AddWithValue("@RazonSocial", RazonSocial);
+                    command.Parameters.AddWithValue("@NombreComercial", NombreComercial);
+                    command.Parameters.AddWithValue("@IdGiroComercio", IdGiroComercio);
+                    command.Parameters.AddWithValue("@Portal", Portal);
+                    command.Parameters.AddWithValue("@Persona", Persona);
+                    command.Parameters.AddWithValue("@Usuario_FAl", IdPerfil);
+                    command.Parameters.AddWithValue("@Usuario_FUM", IdPerfil);
+                    command.Parameters.AddWithValue("@IdTipoDeposito", IdTipoDeposito);
+
+                    command.ExecuteNonQuery();
+                    connection.Close();
+
+                    ViewBag.Alert = CommonServices.ShowAlert(Alerts.Success, "Comercio dado de alta correctamente");
+
+                    return RedirectToAction(nameof(Index), new { alert = ViewBag.Alert });
+
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Alert = CommonServices.ShowAlert(Alerts.Danger, ex.Message);
+                    return RedirectToAction(nameof(Index), new { alert = ViewBag.Alert });
+                }
+            }
         }
 
         public object ListadoBancos()
         {
             List<Banco> BancoList = new List<Banco>();
+
             string connectionString = Configuration["ConnectionStrings:ConexionString"];
+
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -113,24 +108,28 @@ namespace OXXO.Controllers
                     SqlDataReader dataReader = command.ExecuteReader();
                     while (dataReader.Read())
                     {
-                        Banco banco = new Banco();
-                        banco.IdBanco = Convert.ToInt32(dataReader["IdBanco"]);
-                        banco.Bancos = Convert.ToString(dataReader["Bancos"]);
+                        Banco banco = new Banco
+                        {
+                            IdBanco = Convert.ToInt32(dataReader["IdBanco"]),
+                            Bancos = Convert.ToString(dataReader["Bancos"])
+                        };
 
                         BancoList.Add(banco);
                     }
                 }
+
                 ViewData["Bancos"] = new SelectList(BancoList.ToList(), "IdBanco", "Bancos");
                 connection.Close();
 
                 return ViewData["Bancos"];
             }
-
         }
         public object ListadoGiroComercial()
         {
             List<GiroComercio> GiroComercioList = new List<GiroComercio>();
+
             string connectionString = Configuration["ConnectionStrings:ConexionString"];
+
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -141,25 +140,29 @@ namespace OXXO.Controllers
                     SqlDataReader dataReader = command.ExecuteReader();
                     while (dataReader.Read())
                     {
-                        GiroComercio giroComercio = new GiroComercio();
-                        giroComercio.IdGiroComercio = Convert.ToInt32(dataReader["IdGiroComercio"]);
-                        giroComercio.GiroComercial = Convert.ToString(dataReader["GiroComercial"]);
+                        GiroComercio giroComercio = new GiroComercio
+                        {
+                            IdGiroComercio = Convert.ToInt32(dataReader["IdGiroComercio"]),
+                            GiroComercial = Convert.ToString(dataReader["GiroComercial"])
+                        };
 
                         GiroComercioList.Add(giroComercio);
                     }
                 }
+
                 ViewData["GiroComercio"] = new SelectList(GiroComercioList.ToList(), "IdGiroComercio", "GiroComercial");
                 connection.Close();
 
                 return ViewData["GiroComercio"];
             }
-
         }
 
         public object ListadoEstatus()
         {
             List<Estatus> EstatusList = new List<Estatus>();
+
             string connectionString = Configuration["ConnectionStrings:ConexionString"];
+
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
@@ -170,19 +173,20 @@ namespace OXXO.Controllers
                     SqlDataReader dataReader = command.ExecuteReader();
                     while (dataReader.Read())
                     {
-                        Estatus estatus = new Estatus();
-                        estatus.IdEstatus = Convert.ToInt32(dataReader["IdEstatus"]);
-                        estatus.Estatus1 = Convert.ToString(dataReader["Estatus"]);
-
+                        Estatus estatus = new Estatus
+                        {
+                            IdEstatus = Convert.ToInt32(dataReader["IdEstatus"]),
+                            Estatus1 = Convert.ToString(dataReader["Estatus"])
+                        };
                         EstatusList.Add(estatus);
                     }
                 }
+
                 ViewData["Estatus"] = new SelectList(EstatusList.ToList(), "IdEstatus", "Estatus");
                 connection.Close();
 
                 return ViewData["Estatus"];
             }
-
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
