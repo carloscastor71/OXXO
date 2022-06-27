a2c27bbdbba10b68978fbade678dddfa692551d2
msalas1 <104784984+msalas1@users.noreply.github.com>
2022-06-27 18:26:07 -0500

    Tablero Inicio

---
+++ OXXO/OXXO/Models/Tablero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.ComponentModel;
+
+
+namespace OXXO.Models
+{
+    public class Tablero
+    {
+       public int id{ get; set; }
+        public string Nombre { get; set; }
+        public string Transaccion { get; set; }
+        public string Monto { get; set; }
+
+    }
+}
+   
